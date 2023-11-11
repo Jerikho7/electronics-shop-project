1a8d169fa70c690a487d8ed43eab4cccb23a5942
Evgeniya <osipova.dr17@yandex.ru>
2023-11-11 11:39:56 +1100

    add pytest.raises

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -21,7 +21,10 @@ def test_repr(phone_for_test):
 
 
 def test_number_sim(phone_for_test):
-    phone_for_test.number_sim = 1
     assert repr(phone_for_test) == "Phone('iPhone', 120000, 5, 1)"
-    phone_for_test.number_sim = 0
-    assert repr(phone_for_test) == ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+
+
+def test_match(phone_for_test):
+    with pytest.raises(ValueError, match=r"Количество физических SIM-карт должно быть целым числом больше нуля"):
+        phone_for_test.number_sim = 0
+        test_number_sim(phone_for_test)
